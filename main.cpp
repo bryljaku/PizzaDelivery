dc5678365f4267fa88c86eafa78ad15578c57232
jbryl7 <jakub.bryl7@gmail.com>
2019-11-16 16:09:25 +0100

    file input

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <fstream>
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char* argv[]) {
+        std::fstream graphFile;
+        
+        int vertice1, vertice2;
+        int sizeOfGivenGraph;
+        if (argc < 2){
+            std::cout<<"No arguments given\n";
+            return 1;
+        }
+        
+        for (int i = 1; i < argc; i++) {
+            if (argc != 2)
+                std::cout<<"test"<<i<<'\n';
+            std::string fileName = argv[i];
+            graphFile.open(fileName);
+            if (!graphFile.good()) {
+                std::cout << "File not found\n";
+                return 3;
+            }
+            
+            graphFile >> sizeOfGivenGraph;
+            if (sizeOfGivenGraph < 4)
+                break;
+            while (!graphFile.eof()) {
+                graphFile >> vertice1 >> vertice2;
+            }
+            
+            graphFile.close();
+            return 0;
+}
 }
