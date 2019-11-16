302ad968e621ba53ddbaed54cc805eef370288ae
swierczj <kub9818@gmail.com>
2019-11-16 16:18:14 +0100

    test cpp

--- Graph.h
+++ Graph.h
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <queue>
 
+
 #ifndef GRAFY_GRAPH_H
 #define GRAFY_GRAPH_H
 
-class Edge {
-public:
+struct Edge {
     int vertice1, vertice2;
     int cost;
     bool toVisit;
     Edge(int v1, int v2, int c, bool toVis = false): vertice1(v1), vertice2(v2), cost(c), toVisit(toVis) {};
+    bool operator==(const Edge& rhs) const { return( vertice1 == rhs.vertice1 && vertice2 == rhs.vertice2 && cost == rhs.cost && toVisit == rhs.toVisit ); }
 };
 
 class Graph
@@ -20,26 +21,27 @@ private:
 
 public:
     Graph(int givenSize) : neighboursOfEdges(std::vector<std::vector<int>>(givenSize))
-    {}
+        {}
 
     void addEdge(int vertice1, int vertice2, int cost, bool toVisit)
     {
         if (vertice1 == vertice2)
             return;
-        Edge newEdge = new Edge(vertice1, vertice2, cost, toVisit);
+        Edge* newEdge = new Edge(vertice1, vertice2, cost, toVisit);
 
         for (auto edge : allEdges)
-            if (edge == newEdge)
+            if (edge == *newEdge)
                 return;
 
         allEdges.emplace_back(newEdge);
         neighboursOfEdges[vertice1].emplace_back(vertice2);
         neighboursOfEdges[vertice2].emplace_back(vertice1);
     }
+
     void printEdges()
     {
         for (auto edge : allEdges)
-            std::cout<<edge.first<<' '<< edge.second<<'\n';
+            std::cout<< edge.vertice1 <<' '<< edge.vertice2 << '\n';
         std::cout<<"\n";
     }
 
